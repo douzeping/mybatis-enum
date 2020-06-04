5d6f8286a036f1440daa4a69547214c8b5545e01
zpdou <zpdou@dfocuspace.com>
2020-06-04 17:28:59 +0800

    feat: generator plugin

---
+++ src/main/java/tech/zeping/mybatisenum/enums/UserState.java
@@ -0,0 +1,16 @@
+package tech.zeping.mybatisenum.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserState {
+    UNAUTHENTICATED(100, "未验证"), AUTHENTICATED(200, "已验证");
+
+    private Integer code;
+    private String msg;
+
+    UserState(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
